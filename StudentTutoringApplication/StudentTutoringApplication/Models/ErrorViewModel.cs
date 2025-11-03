d543e565c5dc502f3dd67b859ac108419ae072f3
AnotherYetAgain <cjjackson1996@hotmail.com>
2025-11-03 18:14:14 -0400

    Created initial project files.
    Also included EntityFrameworkCore, Tools, SqlServer and Design packages.

---
+++ StudentTutoringApplication/StudentTutoringApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentTutoringApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
